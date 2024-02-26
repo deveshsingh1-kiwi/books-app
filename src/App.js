12e039ed99699ff43a4dfaa6ffd7ebb31504b10f
deveshsingh1-kiwi <devesh.singh1@kiwitech.com>
2024-02-26 18:46:09 +0530

    UI changes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>hello</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to add book with:", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
